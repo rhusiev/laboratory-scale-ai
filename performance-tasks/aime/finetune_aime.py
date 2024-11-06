1dc3466fbfef7c5ce906ffe1992b91c81e9510df
rhusiev <63148080+rhusiev@users.noreply.github.com>
2024-11-06 11:40:20 +0200

    reduce max seq length for ft a bit

--- performance-tasks/aime/finetune_aime.py
+++ performance-tasks/aime/finetune_aime.py
@@ -118,7 +118,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--max_seq_length",
         type=int,
-        default=4096,
+        default=3072,
         help="The maximum sequence length to use for fine-tuning.",
     )
 
