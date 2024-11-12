44582eda94b458679607af60acea9d90d2cdfc47
rhusiev <63148080+rhusiev@users.noreply.github.com>
2024-11-12 13:14:12 +0200

    print images

--- performance-tasks/aime/evaluate_scienceqa.py
+++ performance-tasks/aime/evaluate_scienceqa.py
@@ -1,4 +1,5 @@
 from collections.abc import Sequence
+import time
 import numpy as np
 import json
 import argparse
@@ -280,7 +281,7 @@ if __name__ == "__main__":
     print("Loading dataset")
     data = load_dataset("derek-thomas/ScienceQA", split="test")
     # filter out where image is None
-    data = data.filter(lambda x: bool(x["image"]) and x["image"] != "null")
+    data = data.filter(lambda x: bool(x["image"]) and x["image"] != "null" and time.sleep(1) and printe(x["image"]))
 
     if args.few_shot == "no":
         template = [template[0]]
