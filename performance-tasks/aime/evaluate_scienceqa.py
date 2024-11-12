a22fc080b1d3a9739fe03db574be205965367f8c
rhusiev <63148080+rhusiev@users.noreply.github.com>
2024-11-12 13:19:19 +0200

    print bool of all now

--- performance-tasks/aime/evaluate_scienceqa.py
+++ performance-tasks/aime/evaluate_scienceqa.py
@@ -281,7 +281,7 @@ if __name__ == "__main__":
     print("Loading dataset")
     data = load_dataset("derek-thomas/ScienceQA", split="test")
     # filter out where image is None
-    data = data.filter(lambda x: (print(x["image"]) or time.sleep(1)) and bool(x["image"]) and x["image"] != "null")
+    data = data.filter(lambda x: (print(bool(x["image"])) or time.sleep(1)) and bool(x["image"]) and x["image"] != "null")
 
     if args.few_shot == "no":
         template = [template[0]]
