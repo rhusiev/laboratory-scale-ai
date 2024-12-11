fab7b1ed041a9f3305e0ef12847898e3d10421d1
rhusiev <63148080+rhusiev@users.noreply.github.com>
2024-12-11 19:06:53 +0200

    undo clarify llama that it should complete the step

--- performance-tasks/aime/evaluate_steps.py
+++ performance-tasks/aime/evaluate_steps.py
@@ -179,7 +179,7 @@ def evaluate_hf_model_aime(
         prompt = template + [
             {
                 "role": "user",
-                "content": f"{question}\n\n# To work this out I would first do the following step\n\n{data[idx]['step1']}\n\n# Your task\n\nComplete this step, and I will give you the next instruction.",
+                "content": f"{question}\n\n# To work this out I would first do the following step\n\n{data[idx]['step1']}\n\n# Your task\n\nDo this step, and I will give you the next instruction.",
             }
         ]
         while True:
