59e51598c5226fb6896b052f0163d61c158aeaab
rhusiev <63148080+rhusiev@users.noreply.github.com>
2024-12-11 18:29:04 +0200

    Clarify openai not to mention user's message

--- performance-tasks/aime/evaluate_steps.py
+++ performance-tasks/aime/evaluate_steps.py
@@ -32,7 +32,7 @@ eval_template = {
     "content": "You are a mathematics assistant that helps solve AIME problems. "
     "When asked to give a reasoning step, explain it thoroughly. "
     "When asked to validate user's reasoning step, answer with just a word 'Yes' if the reasoning step is correct. "
-    "Otherwise write a correct reasoning step yourself",
+    "Otherwise write a correct reasoning step yourself, without mentioning the user's step.",
 }
 
 #####
@@ -190,7 +190,7 @@ def evaluate_hf_model_aime(
             steps[-1].append(decoded[-1])
             i += 1
             # to account for limited context size
-            if i >= 7 or idx == 7 and i >= 5:
+            if i >= 6 or idx == 7 and i >= 4:
                 decoded = (
                     decoded[: 2 * (i - 6) + 2]
                     + [
@@ -208,7 +208,7 @@ def evaluate_hf_model_aime(
                     + [
                         {
                             "role": "user",
-                            "content": f"# I think of doing the following reasoning step\n\n{data[idx][f'step{i}']}\n\n# Here is my thought process of completing the step\n\n{decoded[-1]['content']}\n\n# Your task\n\nCheck whether my reasoning step is correct. If it is not, provide your corrected reasoning step. Otherwise reply with 'Yes'",
+                            "content": f"# I think of doing the following reasoning step\n\n{data[idx][f'step{i}']}\n\n# Here is my thought process of completing the step\n\n{decoded[-1]['content']}\n\n# Your task\n\nCheck whether my reasoning step is correct. If it is not, provide your reasoning step without any relation to mine. Otherwise reply with 'Yes'",
                         }
                     ]
                 )
